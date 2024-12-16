8764def329dda726bf323ae840807ef7fbccf4b4
Rinaldes Duma <code@rinal.dev>
2024-12-16 16:56:13 +0700

    📦️ package: add Husky for pre-commit pre-push pre-checkout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,23 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  compatibilityDate: "2024-12-16",
+  compatibilityDate: '2024-12-16',
   future: {
     compatibilityVersion: 4,
   },
 
-  srcDir: "src",
+  srcDir: 'src',
   dir: {
-    pages: "app/pages",
-    layouts: "app/layouts",
+    pages: 'app/pages',
+    layouts: 'app/layouts',
   },
 
   modules: [
-    "@nuxt/ui",
-    "@nuxt/image",
-    "@nuxt/eslint",
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/test-utils/module',
   ],
 });

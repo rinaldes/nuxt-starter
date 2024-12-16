df97127f9e8c829c018f34044db18c01b2e5eeb6
Rinaldes Duma <code@rinal.dev>
2024-12-16 14:26:52 +0700

    🎉 init: Initial project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  compatibilityDate: "2024-12-16",
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  srcDir: "src",
+  dir: {
+    pages: "app/pages",
+    layouts: "app/layouts",
+  },
+
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/eslint"],
+});

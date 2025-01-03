f64fc1a013e0538528f0cdf436f795c51894486a
Rinaldes Duma <code@rinal.dev>
2025-01-03 19:09:21 +0700

    chore: :package: add sentry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: '2024-12-16',
+
   future: {
     compatibilityVersion: 4,
   },
@@ -12,18 +13,36 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@pinia/nuxt',
     '@vueuse/nuxt',
+    '@sentry/nuxt/module',
   ],
 
   srcDir: 'src',
+
   dir: {
     pages: 'app/pages',
     layouts: 'app/layouts',
   },
-  css: ['~/assets/css/main.css'],
+
+  css: ['~/shared/assets/css/main.css'],
+
   components: {
     dirs: ['feature', 'shared/components'],
   },
+
   imports: {
     dirs: ['shared/composables', 'shared/stores', 'shared/utils'],
+  },
+
+  sentry: {
+    sourceMapsUploadOptions: {
+      org: 'handsome-boi-hr',
+      project: 'javascript-nuxt',
+    },
+
+    autoInjectServerSentry: 'top-level-import',
+  },
+
+  sourcemap: {
+    client: 'hidden',
   },
 });
---
+++ src/app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="py-8">
+    <h1 class="mb-4 text-2xl font-bold">Welcome to Starter</h1>
+    <Counter />
+  </div>
+</template>
